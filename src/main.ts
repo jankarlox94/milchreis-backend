68a4d8d7b2d10ea4785e1a28f44b0cc6c20735e9
jankarlox94 <giancarlosanchez.dev@icloud.com>
2026-02-17 19:22:08 -0600

    added new cors origin

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
+      'https://euphonious-dusk-f06a4a.netlify.app',
       'http://localhost:5173', // Your local Vite/React frontend
       'http://localhost:3000',
       'http://[::1]:3000',
@@ -13,6 +14,6 @@ async function bootstrap() {
     credentials: true,
   });
   // app.use(express.json());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
